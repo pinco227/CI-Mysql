265b8a0782d792a14ffb5348ad9c35086800f748
Paul Istratoaie <p4ul.gg@gmail.com>
2021-03-30 23:52:39 +0100

    select, create table and insert sql queries from python

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,7 +1,7 @@
 # import os
 # import pwd
 # pwd.getpwuid(os.getuid())[0]
-
+import datetime
 import pymysql
 
 # Get username
@@ -14,13 +14,33 @@ connection = pymysql.connect(host='localhost',
                              password=password,
                              db="Chinook")
 
+# try:
+#     # Run a query
+#     with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+#         sql = "SELECT * FROM Genre;"
+#         cursor.execute(sql)
+#         for row in cursor:
+#             print(row)
+# finally:
+#     # Close the connection, regardless of whether the above was successful
+#     connection.close()
+
+# try:
+#     # Run a query
+#     with connection.cursor() as cursor:
+#         cursor.execute("""CREATE TABLE IF NOT EXISTS
+#                        Friends(name char(20), age int, DOB datetime);""")
+#         # Note that the above will still display a warning (not error) if the table already exists
+# finally:
+#     # Close the connection, regardless of whether the above was successful
+#     connection.close()
+
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        row = ("Bob", 21, "1990-02-06 23:04:56")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
 finally:
     # Close the connection, regardless of whether the above was successful
     connection.close()
